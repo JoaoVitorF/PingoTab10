8450a250e384046198fe98c659ba96fde193209c
JoaoVitorF <54038578+JoaoVitorF@users.noreply.github.com>
2023-07-24 01:06:02 -0300

    Primeira versão
    Primeiro commit com tudo inicial do programa

---
+++ Programa1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace PingoTabs10
+{
+    partial class Programa1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExplicacaoTxt = new System.Windows.Forms.Label();
+            this.AddSiteTxt = new System.Windows.Forms.Label();
+            this.SiteToAddTxB = new System.Windows.Forms.TextBox();
+            this.Site1Btn = new System.Windows.Forms.Button();
+            this.AddSiteBtn = new System.Windows.Forms.Button();
+            this.Site2Btn = new System.Windows.Forms.Button();
+            this.Site3Btn = new System.Windows.Forms.Button();
+            this.Site4Btn = new System.Windows.Forms.Button();
+            this.Site5Btn = new System.Windows.Forms.Button();
+            this.Site6Btn = new System.Windows.Forms.Button();
+            this.Site7Btn = new System.Windows.Forms.Button();
+            this.Site8Btn = new System.Windows.Forms.Button();
+            this.AvisosLabel = new System.Windows.Forms.Label();
+            this.DeleteSite1 = new System.Windows.Forms.Button();
+            this.DeleteSite2 = new System.Windows.Forms.Button();
+            this.DeleteSite3 = new System.Windows.Forms.Button();
+            this.DeleteSite4 = new System.Windows.Forms.Button();
+            this.DeleteSite5 = new System.Windows.Forms.Button();
+            this.DeleteSite6 = new System.Windows.Forms.Button();
+            this.DeleteSite7 = new System.Windows.Forms.Button();
+            this.DeleteSite8 = new System.Windows.Forms.Button();
+            this.UpdateList = new System.Windows.Forms.Button();
+            this.OpenAllBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ExplicacaoTxt
+            // 
+            this.ExplicacaoTxt.AutoSize = true;
+            this.ExplicacaoTxt.Location = new System.Drawing.Point(13, 13);
+            this.ExplicacaoTxt.Name = "ExplicacaoTxt";
+            this.ExplicacaoTxt.Size = new System.Drawing.Size(76, 15);
+            this.ExplicacaoTxt.TabIndex = 0;
+            this.ExplicacaoTxt.Text = "Sites abertos:";
+            this.ExplicacaoTxt.Click += new System.EventHandler(this.ExplicacaoTxt_Click);
+            // 
+            // AddSiteTxt
+            // 
+            this.AddSiteTxt.AutoSize = true;
+            this.AddSiteTxt.Location = new System.Drawing.Point(617, 26);
+            this.AddSiteTxt.Name = "AddSiteTxt";
+            this.AddSiteTxt.Size = new System.Drawing.Size(99, 15);
+            this.AddSiteTxt.TabIndex = 1;
+            this.AddSiteTxt.Text = "Adicione um site:";
+            this.AddSiteTxt.Click += new System.EventHandler(this.AddSiteTxt_Click);
+            // 
+            // SiteToAddTxB
+            // 
+            this.SiteToAddTxB.Location = new System.Drawing.Point(562, 44);
+            this.SiteToAddTxB.Name = "SiteToAddTxB";
+            this.SiteToAddTxB.Size = new System.Drawing.Size(206, 23);
+            this.SiteToAddTxB.TabIndex = 2;
+            this.SiteToAddTxB.TextChanged += new System.EventHandler(this.SiteToAddTxB_TextChanged);
+            // 
+            // Site1Btn
+            // 
+            this.Site1Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site1Btn.Location = new System.Drawing.Point(13, 31);
+            this.Site1Btn.Name = "Site1Btn";
+            this.Site1Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site1Btn.TabIndex = 3;
+            this.Site1Btn.UseVisualStyleBackColor = true;
+            this.Site1Btn.Click += new System.EventHandler(this.Site1Btn_Click);
+            // 
+            // AddSiteBtn
+            // 
+            this.AddSiteBtn.Location = new System.Drawing.Point(763, 43);
+            this.AddSiteBtn.Name = "AddSiteBtn";
+            this.AddSiteBtn.Size = new System.Drawing.Size(25, 24);
+            this.AddSiteBtn.TabIndex = 4;
+            this.AddSiteBtn.Text = "+";
+            this.AddSiteBtn.UseVisualStyleBackColor = true;
+            this.AddSiteBtn.Click += new System.EventHandler(this.AddSiteBtn_Click);
+            // 
+            // Site2Btn
+            // 
+            this.Site2Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site2Btn.Location = new System.Drawing.Point(13, 83);
+            this.Site2Btn.Name = "Site2Btn";
+            this.Site2Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site2Btn.TabIndex = 5;
+            this.Site2Btn.UseVisualStyleBackColor = true;
+            this.Site2Btn.Click += new System.EventHandler(this.Site2Btn_Click);
+            // 
+            // Site3Btn
+            // 
+            this.Site3Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site3Btn.Location = new System.Drawing.Point(13, 135);
+            this.Site3Btn.Name = "Site3Btn";
+            this.Site3Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site3Btn.TabIndex = 6;
+            this.Site3Btn.UseVisualStyleBackColor = true;
+            this.Site3Btn.Click += new System.EventHandler(this.Site3Btn_Click);
+            // 
+            // Site4Btn
+            // 
+            this.Site4Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site4Btn.Location = new System.Drawing.Point(12, 187);
+            this.Site4Btn.Name = "Site4Btn";
+            this.Site4Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site4Btn.TabIndex = 7;
+            this.Site4Btn.UseVisualStyleBackColor = true;
+            this.Site4Btn.Click += new System.EventHandler(this.Site4Btn_Click);
+            // 
+            // Site5Btn
+            // 
+            this.Site5Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site5Btn.Location = new System.Drawing.Point(13, 239);
+            this.Site5Btn.Name = "Site5Btn";
+            this.Site5Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site5Btn.TabIndex = 8;
+            this.Site5Btn.UseVisualStyleBackColor = true;
+            this.Site5Btn.Click += new System.EventHandler(this.Site5Btn_Click);
+            // 
+            // Site6Btn
+            // 
+            this.Site6Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site6Btn.Location = new System.Drawing.Point(13, 291);
+            this.Site6Btn.Name = "Site6Btn";
+            this.Site6Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site6Btn.TabIndex = 9;
+            this.Site6Btn.UseVisualStyleBackColor = true;
+            this.Site6Btn.Click += new System.EventHandler(this.Site6Btn_Click);
+            // 
+            // Site7Btn
+            // 
+            this.Site7Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site7Btn.Location = new System.Drawing.Point(13, 343);
+            this.Site7Btn.Name = "Site7Btn";
+            this.Site7Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site7Btn.TabIndex = 10;
+            this.Site7Btn.UseVisualStyleBackColor = true;
+            this.Site7Btn.Click += new System.EventHandler(this.Site7Btn_Click);
+            // 
+            // Site8Btn
+            // 
+            this.Site8Btn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Site8Btn.Location = new System.Drawing.Point(13, 392);
+            this.Site8Btn.Name = "Site8Btn";
+            this.Site8Btn.Size = new System.Drawing.Size(472, 46);
+            this.Site8Btn.TabIndex = 11;
+            this.Site8Btn.UseVisualStyleBackColor = true;
+            this.Site8Btn.Click += new System.EventHandler(this.Site8Btn_Click);
+            // 
+            // AvisosLabel
+            // 
+            this.AvisosLabel.AutoSize = true;
+            this.AvisosLabel.Location = new System.Drawing.Point(562, 99);
+            this.AvisosLabel.Name = "AvisosLabel";
+            this.AvisosLabel.Size = new System.Drawing.Size(44, 15);
+            this.AvisosLabel.TabIndex = 12;
+            this.AvisosLabel.Text = "Avisos:";
+            this.AvisosLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DeleteSite1
+            // 
+            this.DeleteSite1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite1.Location = new System.Drawing.Point(478, 31);
+            this.DeleteSite1.Name = "DeleteSite1";
+            this.DeleteSite1.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite1.TabIndex = 13;
+            this.DeleteSite1.Text = "-";
+            this.DeleteSite1.UseVisualStyleBackColor = true;
+            this.DeleteSite1.Click += new System.EventHandler(this.DeleteSite1_Click);
+            // 
+            // DeleteSite2
+            // 
+            this.DeleteSite2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite2.Location = new System.Drawing.Point(478, 83);
+            this.DeleteSite2.Name = "DeleteSite2";
+            this.DeleteSite2.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite2.TabIndex = 14;
+            this.DeleteSite2.Text = "-";
+            this.DeleteSite2.UseVisualStyleBackColor = true;
+            this.DeleteSite2.Click += new System.EventHandler(this.DeleteSite2_Click);
+            // 
+            // DeleteSite3
+            // 
+            this.DeleteSite3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite3.Location = new System.Drawing.Point(478, 135);
+            this.DeleteSite3.Name = "DeleteSite3";
+            this.DeleteSite3.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite3.TabIndex = 15;
+            this.DeleteSite3.Text = "-";
+            this.DeleteSite3.UseVisualStyleBackColor = true;
+            this.DeleteSite3.Click += new System.EventHandler(this.DeleteSite3_Click);
+            // 
+            // DeleteSite4
+            // 
+            this.DeleteSite4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite4.Location = new System.Drawing.Point(478, 187);
+            this.DeleteSite4.Name = "DeleteSite4";
+            this.DeleteSite4.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite4.TabIndex = 16;
+            this.DeleteSite4.Text = "-";
+            this.DeleteSite4.UseVisualStyleBackColor = true;
+            this.DeleteSite4.Click += new System.EventHandler(this.DeleteSite4_Click);
+            // 
+            // DeleteSite5
+            // 
+            this.DeleteSite5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite5.Location = new System.Drawing.Point(478, 239);
+            this.DeleteSite5.Name = "DeleteSite5";
+            this.DeleteSite5.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite5.TabIndex = 17;
+            this.DeleteSite5.Text = "-";
+            this.DeleteSite5.UseVisualStyleBackColor = true;
+            this.DeleteSite5.Click += new System.EventHandler(this.DeleteSite5_Click);
+            // 
+            // DeleteSite6
+            // 
+            this.DeleteSite6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite6.Location = new System.Drawing.Point(478, 291);
+            this.DeleteSite6.Name = "DeleteSite6";
+            this.DeleteSite6.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite6.TabIndex = 18;
+            this.DeleteSite6.Text = "-";
+            this.DeleteSite6.UseVisualStyleBackColor = true;
+            this.DeleteSite6.Click += new System.EventHandler(this.DeleteSite6_Click);
+            // 
+            // DeleteSite7
+            // 
+            this.DeleteSite7.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite7.Location = new System.Drawing.Point(478, 343);
+            this.DeleteSite7.Name = "DeleteSite7";
+            this.DeleteSite7.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite7.TabIndex = 19;
+            this.DeleteSite7.Text = "-";
+            this.DeleteSite7.UseVisualStyleBackColor = true;
+            this.DeleteSite7.Click += new System.EventHandler(this.DeleteSite7_Click);
+            // 
+            // DeleteSite8
+            // 
+            this.DeleteSite8.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.DeleteSite8.Location = new System.Drawing.Point(478, 392);
+            this.DeleteSite8.Name = "DeleteSite8";
+            this.DeleteSite8.Size = new System.Drawing.Size(43, 46);
+            this.DeleteSite8.TabIndex = 20;
+            this.DeleteSite8.Text = "-";
+            this.DeleteSite8.UseVisualStyleBackColor = true;
+            this.DeleteSite8.Click += new System.EventHandler(this.DeleteSite8_Click);
+            // 
+            // UpdateList
+            // 
+            this.UpdateList.Location = new System.Drawing.Point(562, 135);
+            this.UpdateList.Name = "UpdateList";
+            this.UpdateList.Size = new System.Drawing.Size(226, 23);
+            this.UpdateList.TabIndex = 21;
+            this.UpdateList.Text = "Atualizar lista";
+            this.UpdateList.UseVisualStyleBackColor = true;
+            this.UpdateList.Click += new System.EventHandler(this.UpdateList_Click);
+            // 
+            // OpenAllBtn
+            // 
+            this.OpenAllBtn.Location = new System.Drawing.Point(295, 5);
+            this.OpenAllBtn.Name = "OpenAllBtn";
+            this.OpenAllBtn.Size = new System.Drawing.Size(226, 23);
+            this.OpenAllBtn.TabIndex = 22;
+            this.OpenAllBtn.Text = "Abrir todos em uma janela";
+            this.OpenAllBtn.UseVisualStyleBackColor = true;
+            this.OpenAllBtn.Click += new System.EventHandler(this.OpenAllBtn_Click);
+            // 
+            // Programa1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OpenAllBtn);
+            this.Controls.Add(this.UpdateList);
+            this.Controls.Add(this.DeleteSite8);
+            this.Controls.Add(this.DeleteSite7);
+            this.Controls.Add(this.DeleteSite6);
+            this.Controls.Add(this.DeleteSite5);
+            this.Controls.Add(this.DeleteSite4);
+            this.Controls.Add(this.DeleteSite3);
+            this.Controls.Add(this.DeleteSite2);
+            this.Controls.Add(this.DeleteSite1);
+            this.Controls.Add(this.AvisosLabel);
+            this.Controls.Add(this.Site8Btn);
+            this.Controls.Add(this.Site7Btn);
+            this.Controls.Add(this.Site6Btn);
+            this.Controls.Add(this.Site5Btn);
+            this.Controls.Add(this.Site4Btn);
+            this.Controls.Add(this.Site3Btn);
+            this.Controls.Add(this.Site2Btn);
+            this.Controls.Add(this.AddSiteBtn);
+            this.Controls.Add(this.Site1Btn);
+            this.Controls.Add(this.SiteToAddTxB);
+            this.Controls.Add(this.AddSiteTxt);
+            this.Controls.Add(this.ExplicacaoTxt);
+            this.Name = "Programa1";
+            this.Text = "PinguTabs10- Gerenciador de abas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ExplicacaoTxt;
+        private System.Windows.Forms.Label AddSiteTxt;
+        private System.Windows.Forms.TextBox SiteToAddTxB;
+        private System.Windows.Forms.Button Site1Btn;
+        private System.Windows.Forms.Button AddSiteBtn;
+        private System.Windows.Forms.Button Site2Btn;
+        private System.Windows.Forms.Button Site3Btn;
+        private System.Windows.Forms.Button Site4Btn;
+        private System.Windows.Forms.Button Site5Btn;
+        private System.Windows.Forms.Button Site6Btn;
+        private System.Windows.Forms.Button Site7Btn;
+        private System.Windows.Forms.Button Site8Btn;
+        private System.Windows.Forms.Label AvisosLabel;
+        private System.Windows.Forms.Button DeleteSite1;
+        private System.Windows.Forms.Button DeleteSite2;
+        private System.Windows.Forms.Button DeleteSite3;
+        private System.Windows.Forms.Button DeleteSite4;
+        private System.Windows.Forms.Button DeleteSite5;
+        private System.Windows.Forms.Button DeleteSite6;
+        private System.Windows.Forms.Button DeleteSite7;
+        private System.Windows.Forms.Button DeleteSite8;
+        private System.Windows.Forms.Button UpdateList;
+        private System.Windows.Forms.Button OpenAllBtn;
+    }
+}
+
---
+++ Programa1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace PingoTabs10
+{
+    public partial class Programa1 : Form
+    {
+        string[] Sites = new string[8];
+        public string Savepath = Environment.CurrentDirectory + "/" + "save.txt";
+
+        public void CreateSave()
+        {
+            StreamWriter sw = new StreamWriter(Savepath);
+            sw.Close();
+        }
+
+        public void LoadSave()
+        {
+            Sites[0] = GetLine(Savepath,0);
+            Sites[1] = GetLine(Savepath,1);
+            Sites[2] = GetLine(Savepath,2);
+            Sites[3] = GetLine(Savepath,3);
+            Sites[4] = GetLine(Savepath,4);
+            Sites[5] = GetLine(Savepath,5);
+            Sites[6] = GetLine(Savepath,6);
+            Sites[7] = GetLine(Savepath,7);
+            UpdateSites();
+        }
+
+        public string LoadSaveComplete()
+        {
+            StreamReader sr = new StreamReader(Savepath);
+            string load = sr.ReadToEnd();
+            sr.Close();
+            return load;
+        }
+
+        public void Salvar(string web)
+        {
+            string oldsave = LoadSaveComplete();
+            StreamWriter sw = new StreamWriter(Savepath);
+            sw.WriteLine(oldsave + web);
+            sw.Close();
+        }
+
+        public Programa1()
+        {
+            InitializeComponent();
+            if (!File.Exists(Savepath))
+            {
+                CreateSave();
+                MessageBox.Show("New Save created sucess");
+            }
+            else
+            {
+                LoadSave();
+            }
+        }
+
+        private void AddSiteTxt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SiteToAddTxB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExplicacaoTxt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddSiteBtn_Click(object sender, EventArgs e)
+        {
+            if (Sites[7] != null)
+            {
+                AvisosLabel.Text = "Lotado";
+            }
+            else
+            {
+                for (int i = 0; i <= Sites.Length - 1; i++)
+                {
+                    if (Sites[i] == null)
+                    {
+                        Sites[i] = SiteToAddTxB.Text;
+                        Salvar(Sites[i]);
+                        i = Sites.Length;
+                    }
+                }
+                UpdateSites();
+            }
+        }
+
+        private void UpdateSites()
+        {
+            Site1Btn.Text = Sites[0];
+            Site2Btn.Text = Sites[1];
+            Site3Btn.Text = Sites[2];
+            Site4Btn.Text = Sites[3];
+            Site5Btn.Text = Sites[4];
+            Site6Btn.Text = Sites[5];
+            Site7Btn.Text = Sites[6];
+            Site8Btn.Text = Sites[7];
+        }
+
+        private void Site1Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site1Btn.Text;
+            Process.Start(prs);
+        }
+
+        private void Site2Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site2Btn.Text;
+            Process.Start(prs);
+        }
+
+        private void Site3Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site3Btn.Text;
+            Process.Start(prs);
+        }
+
+        private void Site4Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site4Btn.Text;
+            Process.Start(prs);
+        }
+
+        private void Site5Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site5Btn.Text;
+            Process.Start(prs);
+        }
+
+        private void Site6Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site6Btn.Text;
+            Process.Start(prs);
+        }
+
+        private void Site7Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site7Btn.Text;
+            Process.Start(prs);
+        }
+
+        private void Site8Btn_Click(object sender, EventArgs e)
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site8Btn.Text;
+            Process.Start(prs);
+        }
+
+        
+
+        string GetLine(string fileName, int line)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                for (int i = 1; i <= line; i++)
+                    sr.ReadLine();
+                return sr.ReadLine();
+            }
+        }
+
+        static void lineChanger(string newText, string fileName, int line_to_edit)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+            arrLine[line_to_edit - 1] = newText;
+            File.WriteAllLines(fileName, arrLine);
+        }
+
+        static void LineDelete(string fileName, int line_to_delete)
+        {
+            var file = new List<string>(System.IO.File.ReadAllLines(fileName));
+            file.RemoveAt(line_to_delete);
+            File.WriteAllLines(fileName, file.ToArray());
+        }
+
+        private void UpdateList_Click(object sender, EventArgs e)
+        {
+            LoadSave();
+        }
+
+        private void DeleteSite1_Click(object sender, EventArgs e)
+        {
+            Site1Btn.Text = "";
+            Sites[0] = "";
+            LineDelete(Savepath, 0);
+            UpdateSites();
+        }
+
+        private void DeleteSite2_Click(object sender, EventArgs e)
+        {
+            Site2Btn.Text = "";
+            Sites[1] = "";
+            LineDelete(Savepath, 1);
+            UpdateSites();
+        }
+
+        private void DeleteSite3_Click(object sender, EventArgs e)
+        {
+            Site2Btn.Text = "";
+            Sites[2] = "";
+            LineDelete(Savepath, 2);
+            UpdateSites();
+        }
+
+        private void DeleteSite4_Click(object sender, EventArgs e)
+        {
+            Site2Btn.Text = "";
+            Sites[3] = "";
+            LineDelete(Savepath, 3);
+            UpdateSites();
+        }
+
+        private void DeleteSite5_Click(object sender, EventArgs e)
+        {
+            Site2Btn.Text = "";
+            Sites[4] = "";
+            LineDelete(Savepath, 4);
+            UpdateSites();
+        }
+
+        private void DeleteSite6_Click(object sender, EventArgs e)
+        {
+            Site2Btn.Text = "";
+            Sites[5] = "";
+            LineDelete(Savepath, 5);
+            UpdateSites();
+        }
+
+        private void DeleteSite7_Click(object sender, EventArgs e)
+        {
+            Site2Btn.Text = "";
+            Sites[6] = "";
+            LineDelete(Savepath, 6);
+            UpdateSites();
+        }
+
+        private void DeleteSite8_Click(object sender, EventArgs e)
+        {
+            Site2Btn.Text = "";
+            Sites[7] = "";
+            LineDelete(Savepath, 7);
+            UpdateSites();
+        }
+
+        private void OpenAllBtn_Click(object sender, EventArgs e)
+        {
+            NovaJanela();
+            //NovaAba();
+        }
+
+        private void NovaJanela()
+        {
+            LoadSave();
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            Process.Start(prs);
+            for(int b = 0; b <= Sites.Length-1;b++)
+            {
+                if (Sites[b] != null)
+                {
+                    prs.Arguments = Sites[b];
+                    Process.Start(prs);
+                    AvisosLabel.Text = Sites[b];
+                }
+            }
+        }
+
+        private void NovaAba()
+        {
+            var prs = new ProcessStartInfo("C:/Program Files/Google/Chrome/Application/chrome.exe");
+            prs.Arguments = Site6Btn.Text;
+            Process.Start(prs);
+        }
+    }
+}
